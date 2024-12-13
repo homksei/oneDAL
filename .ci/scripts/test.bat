c019591efcdabaca5b747cac9d38d83072f3082c
homksei <aleksei.khomenko@intel.com>
2024-12-13 14:45:53 +0100

    ci: move checks under actions

--- .ci/scripts/test.bat
+++ .ci/scripts/test.bat
@@ -3,7 +3,7 @@ rem ============================================================================
 rem Copyright 2022 Intel Corporation
 rem
 rem Licensed under the Apache License, Version 2.0 (the "License");
-rem you may not use this file except in compliance with the License.
+rem you may not use thdis file except in compliance with the License.
 rem You may obtain a copy of the License at
 rem
 rem     http://www.apache.org/licenses/LICENSE-2.0
